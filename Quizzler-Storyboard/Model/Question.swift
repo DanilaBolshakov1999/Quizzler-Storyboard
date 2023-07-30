e27d136eabeec0910d47b047a3b97475f3edf870
Danila Bolshakov <danila1999777@gmail.com>
2023-07-30 19:19:35 +0300

    initial commit

---
+++ Quizzler-Storyboard/Model/Question.swift
@@ -0,0 +1,18 @@
+//
+//  Extensions.swift
+//  Quizzler-Storyboard
+//
+//  Created by iOS - Developer on 30.07.2023.
+//
+
+import UIKit
+
+struct Question {
+    let text: String
+    let answer: String
+    
+    init(text: String, answer: String) {
+        self.text = text
+        self.answer = answer
+    }
+}
